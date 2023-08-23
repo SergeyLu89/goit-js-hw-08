82bb2ed4210aef41451c28594a804b4d0c179bca
SergeyLu89 <lytsenko.89@gmail.com>
2023-08-23 12:10:08 +0300

    task_1_done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,22 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log('test');
+const galeryEl = document.querySelector('.gallery');
+const murkup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`
+);
+galeryEl.insertAdjacentHTML('beforeend', murkup.join(''));
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+galeryEl.style.listStyle = 'none';
