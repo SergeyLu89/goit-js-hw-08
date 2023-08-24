d6259fd090d85896b68ec5c3a8d0d385c1c9df22
SergeyLu89 <lytsenko.89@gmail.com>
2023-08-24 15:56:40 +0300

    task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const vimeoPlyer = document.querySelector('#vimeo-player');
+
+const player = new Player(vimeoPlyer);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    playerTime = localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
