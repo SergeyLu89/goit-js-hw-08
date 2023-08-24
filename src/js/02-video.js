9832fb895743e814346124cb0738c4bf7f315ec9
SergeyLu89 <lytsenko.89@gmail.com>
2023-08-24 17:29:45 +0300

    task_2_change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,21 +8,33 @@ const player = new Player(vimeoPlayer);
 player.on(
   'timeupdate',
   throttle(data => {
-    playerTime = localStorage.setItem('videoplayer-current-time', data.seconds);
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
   }, 1000)
 );
+const playerCurrentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+console.log(playerCurrentTime);
+function checkPlayerCurrentTime() {
+  if (playerCurrentTime !== 0) {
+    player.setCurrentTime(playerCurrentTime);
+  }
+}
+checkPlayerCurrentTime(playerCurrentTime);
+// player
+//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
